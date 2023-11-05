cb0a06035f36f48337c7ba8620db4cb500e024a2
Angl99 <anggelplasencia@pursuit.org>
2023-11-05 12:21:49 -0500

    finished base form and working on js

--- events/main.js
+++ events/main.js
@@ -1,1 +1,14 @@
- 
+const moviesList = [
+    {
+        name: "Matrix",
+        year: 1999,
+        genre: "Sci-fi",
+        img: "imgs/matrix.png",
+        price: 34.99,
+        numAvailable: 10
+    }
+]
+
+function createMovie(params) {
+    
+}
