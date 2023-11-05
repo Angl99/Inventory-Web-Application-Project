1984d2f5fedaa245578ad1e77be13c7aad92e802
Angl99 <anggelplasencia@pursuit.org>
2023-11-05 13:34:26 -0500

    finished base js functionality

--- events/main.js
+++ events/main.js
@@ -5,10 +5,49 @@ const moviesList = [
         genre: "Sci-fi",
         img: "imgs/matrix.png",
         price: 34.99,
-        numAvailable: 10
+        instock: "In stock",
+    },
+    {
+        name: "Dead Space: Downfall",
+        year: 2008,
+        genre: "Horror",
+        img: "imgs/dsd.png",
+        price: 24.99,
+        instock: "Preorder",
+    },
+    {
+        name: "Alien",
+        year: 1979,
+        genre: "Sci-fi",
+        img: "imgs/alien.png",
+        price: 54.99,
+        instock: "Out of Stock",
     }
 ]
 
-function createMovie(params) {
+function createMovie(movie) {
+    const movieContainer = document.createElement("div");
+    movieContainer.classList.add("movie-board");
+
     
+    const movieImageElement = document.createElement("img");
+    movieImageElement.setAttribute("src", movie.img);
+
+    const movieName = document.createElement("strong");
+    movieName.textContent = movie.name;
+
+    const movieYear = document.createElement("p");
+    movieYear.textContent = movie.year;
+
+    const movieGenre = document.createElement("p");
+    movieGenre.textContent = movie.genre;
+
+    const moviePrice = document.createElement("p");
+    moviePrice.textContent = movie.price;
+
+    const movieInStock = document.createElement("p");
+    movieInStock.textContent = movie.instock;
+
+    movieContainer.append(movieImageElement, movieName, movieYear, movieGenre, moviePrice, movieInStock);
+
 }
