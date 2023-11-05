e034800e5d304b1ec9ce1525234cf4cd0dbc582b
Angl99 <anggelplasencia@pursuit.org>
2023-11-05 15:23:51 -0500

    finished js functionality and collecting form data

--- events/main.js
+++ events/main.js
@@ -1,37 +1,33 @@
-const moviesList = [
-    {
-        name: "Matrix",
-        year: 1999,
-        genre: "Sci-fi",
-        img: "imgs/matrix.png",
-        price: 34.99,
-        instock: "In stock",
-    },
-    {
-        name: "Dead Space: Downfall",
-        year: 2008,
-        genre: "Horror",
-        img: "imgs/dsd.png",
-        price: 24.99,
-        instock: "Preorder",
-    },
-    {
-        name: "Alien",
-        year: 1979,
-        genre: "Sci-fi",
-        img: "imgs/alien.png",
-        price: 54.99,
-        instock: "Out of Stock",
-    }
-]
+// const moviesList = [
+//     {
+//         name: "Matrix",
+//         year: 1999,
+//         genre: "Sci-fi",
+//         img: "imgs/matrix.png",
+//         price: 34.99,
+//         instock: "In stock",
+//     },
+//     {
+//         name: "Dead Space: Downfall",
+//         year: 2008,
+//         genre: "Horror",
+//         img: "imgs/dsd.png",
+//         price: 24.99,
+//         instock: "Preorder",
+//     },
+//     {
+//         name: "Alien",
+//         year: 1979,
+//         genre: "Sci-fi",
+//         img: "imgs/alien.png",
+//         price: 54.99,
+//         instock: "Out of Stock",
+//     }
+// ]
 
 function createMovie(movie) {
     const movieContainer = document.createElement("div");
     movieContainer.classList.add("movie-board");
-
-    
-    const movieImageElement = document.createElement("img");
-    movieImageElement.setAttribute("src", movie.img);
 
     const movieName = document.createElement("strong");
     movieName.textContent = movie.name;
@@ -42,12 +38,54 @@ function createMovie(movie) {
     const movieGenre = document.createElement("p");
     movieGenre.textContent = movie.genre;
 
+    const movieImageElement = document.createElement("img");
+    movieImageElement.setAttribute("src", movie.img);
+
     const moviePrice = document.createElement("p");
     moviePrice.textContent = movie.price;
 
     const movieInStock = document.createElement("p");
     movieInStock.textContent = movie.instock;
 
-    movieContainer.append(movieImageElement, movieName, movieYear, movieGenre, moviePrice, movieInStock);
+    movieContainer.append(
+        movieName, 
+        movieYear, 
+        movieGenre, 
+        movieImageElement, 
+        moviePrice, 
+        movieInStock);
+
+const mainSection = document.querySelector("main");
+  mainSection.append(movieContainer);
 
 }
+
+const formElement = document.getElementById("new-movie-form");
+    formElement.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        const name = formElement.movieName.value;
+        const year = formElement.movieYear.value;
+        const genre = formElement.movieGenre.value;
+        const img = formElement.movieImageUrl.value;
+        const price = formElement.moviePrice.value;
+        const inStock = formElement.movieInStock.value;
+        // const errorMsg = document.querySelector("p");
+
+        // if (movieName === "" || movieYear === "" || movieGenre === "" || movieImageUrl === "" || moviePrice === "" || movieInStock === "") {
+        //     errorMsg.textContent = "Please fill in all fields";
+        // }
+
+        formElement.reset();
+
+        createMovie({
+            name,
+            year,
+            genre,
+            img,
+            price,
+            inStock
+        });
+    })
+
+// console.log(movieContainer);
